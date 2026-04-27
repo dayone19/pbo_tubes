f3ae0919487bd529173c9b21910e5184e520cde7
dayone19 <deyoansalsabila43@gmail.com>
2026-04-27 12:45:38 +0700

    yang baru

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
 
-//include dari farah
 #include "farah/Person.cpp"
 #include "farah/Doctor.cpp"
 #include "farah/Patient.cpp"
 #include "farah/StaffAdmin.cpp"
 
-//include dari zerlina
-#include "zerlina/MedicalRecord.cpp"
-#include "zerlina/Prescription.cpp"
+#include "zerlina/DoctorGeneral.cpp"
+#include "zerlina/DoctorSpecialist.cpp"
 
-//include dari elysta
 #include "elysta/Billing.cpp"
-#include "elysta/Appointment.cpp"
-#include "elysta/Diagnosis.cpp"
 
-//include dari core yoan
 #include "yoan/Database.cpp"
-#include "yoan/FileHandler.cpp"
-#include "yoan/CustomException.cpp"
+#include "yoan/Appointment.cpp"
+#include "yoan/MedicalRecord.cpp"
+#include "yoan/Prescription.cpp"
+
+#include "gretha/FileHandler.cpp"
+#include "gretha/CustomException.cpp"
